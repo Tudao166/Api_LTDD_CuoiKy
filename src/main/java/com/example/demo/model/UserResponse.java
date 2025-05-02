49a2d77787b5c7b41687f649697803af25cd14be
tudao164 <daothanhtu2018@gmail.com>
2025-05-02 12:36:10 +0700

    Update cart and cartitem

---
+++ src/main/java/com/example/demo/model/UserResponse.java
@@ -0,0 +1,29 @@
+package com.example.demo.model;
+
+
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+//Đào Thanh Tú - 22110452
+
+
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+public class UserResponse {
+    String password;
+
+    String fullName;
+
+    String email;
+
+    Integer isActive;
+
+    String picture;
+    boolean gender;
+}
